dc873f47df111f9d2b6d4c8a0c31399ea973abb1
Bani <banimontoya@gmail.com>
2023-03-01 11:00:37 -0500

    chore: translate comments to english

--- grades/api/test_answers.py
+++ grades/api/test_answers.py
@@ -13,14 +13,14 @@ class StudentAnswerAPITestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
 
-        # crear un usuario de prueba
+        # create a test user
         self.user = User.objects.create_user(
             username='testuser',
             email='testuser@example.com',
             password='testpass'
         )
 
-        # crear un examen de prueba
+        # create a test exam
         self.test = Test.objects.create(
             title='Test de prueba',
             description='Este es un examen de prueba',
@@ -28,19 +28,20 @@ class StudentAnswerAPITestCase(TestCase):
             end_time='2022-03-02T12:00:00Z'
         )
 
-        # crear una pregunta de prueba para el examen
+        # create a test question for the exam
         self.question = Question.objects.create(
             test=self.test,
             question_text='¿Cuál es la capital de Francia?'
         )
-        # crear un estudiante de prueba
+
+        # create a test student
         self.student = Student.objects.create(
             user=self.user,
             address='123 Main St',
             whatsapp='555-5555'
         )
 
-        # crear una respuesta de estudiante de prueba para la pregunta
+        # create a test student answer for the question
         self.student_answer = StudentAnswer.objects.create(
             question=self.question,
             student=Student.objects.get(user=self.user),
@@ -48,10 +49,10 @@ class StudentAnswerAPITestCase(TestCase):
         )
 
     def test_post_student_answer(self):
-        # autenticar el cliente con el usuario de prueba
+        # authenticate client with test user
         #self.client.force_authenticate(user=self.user)
 
-        # datos de la respuesta de estudiante
+        # data for student answer
         answer_data = {
             'question': self.question.pk,
             'answer_text': 'Londres'
@@ -65,14 +66,14 @@ class StudentAnswerAPITestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('access', response.data)
         access_token = response.data['access']
-    
+
         response = self.client.post(
             reverse('register_answer'), answer_data, HTTP_AUTHORIZATION=f'Bearer {access_token}')
 
-        # comprobar que se recibe una respuesta 201 CREATED
+        # check that a 201 CREATED response is received
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        # comprobar que la respuesta devuelta coincide con la respuesta de estudiante creada anteriormente
+        # check that the returned response matches the test student answer created earlier
         self.assertNotEqual(response.data, StudentAnswerSerializer(
             self.student_answer).data)
         self.assertEqual(response.data['answer_text'],'Londres')
