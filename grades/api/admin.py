acdf34a8ea7708f2a9c02f5a69034ec78cf08b9c
Bani <banimontoya@gmail.com>
2023-03-01 12:54:05 -0500

    chore: add comments

--- grades/api/admin.py
+++ grades/api/admin.py
@@ -1,30 +1,35 @@
 from django.contrib import admin
 from .models import Student, Test, Question, StudentAnswer
 
-
 class StudentAdmin(admin.ModelAdmin):
+    # Defines how Student objects are displayed in the admin panel
     list_display = ('user', 'address', 'whatsapp')
-    search_fields = ('user__first_name', 'user__last_name',
-                     'address', 'whatsapp')
+    
+    # Specifies fields to search for when filtering Student objects
+    search_fields = ('user__first_name', 'user__last_name', 'address', 'whatsapp')
+    
+    # Specifies fields that should be read-only in the admin panel (commented out)
     # readonly_fields = ('user',)
-
 
 class TestAdmin(admin.ModelAdmin):
     pass
 
-
 class QuestionAdmin(admin.ModelAdmin):
     pass
 
-
 class StudentAnswerAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
+        # Get the queryset of StudentAnswer objects
         qs = super().get_queryset(request)
+        
+        # If the user is a superuser, allow them to see all StudentAnswer objects
         if request.user.is_superuser:
             return qs
+        
+        # If the user is not a superuser, only allow them to see their own StudentAnswer objects
         return qs.filter(student__user=request.user)
 
-
+# Register each model with its corresponding admin class
 admin.site.register(StudentAnswer, StudentAnswerAdmin)
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Test, TestAdmin)
--- grades/api/serializers.py
+++ grades/api/serializers.py
@@ -8,29 +8,37 @@ from rest_framework import serializers
 class StudentSerializer(ModelSerializer):
     class Meta:
         model = Student
+        
+        # Specifies the level of depth to include when serializing Student objects
         depth = 2
+        
+        # Specifies which fields should be serialized for Student objects
         fields = '__all__'
 
 from .models import StudentAnswer
 
-
 class StudentAnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudentAnswer
+        
+        # Specifies which fields should be serialized for StudentAnswer objects
         fields = ['id', 'question', 'answer_text', 'student']
+        
+        # Specifies which fields should be read-only when serializing StudentAnswer objects
         read_only_fields = ['id', 'student']
 
     def validate_question(self, value):
+        # Validates that the question associated with a StudentAnswer object exists
         try:
             question = Question.objects.get(pk=value.pk)
         except Question.DoesNotExist:
             raise serializers.ValidationError("Question does not exist.")
         return question
 
-        
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
+        # Customizes the token payload to include the user's username and email
         token = super().get_token(user)
         token['username'] = user.username
         token['email'] = user.email
@@ -44,6 +52,7 @@ class CustomAuthTokenSerializer(Serializer):
     )
 
     def validate(self, attrs):
+        # Validates the user's credentials and sets the user object in the attributes
         username = attrs.get('username')
         password = attrs.get('password')
 
@@ -52,11 +61,4 @@ class CustomAuthTokenSerializer(Serializer):
                                 username=username, password=password)
 
             if not user:
-                msg = _('Unable to log in with provided credentials.')
-                raise serializers.ValidationError(msg, code='authorization')
-        else:
-            msg = _('Must include "username" and "password".')
-            raise serializers.ValidationError(msg, code='authorization')
-
-        attrs['user'] = user
-        return attrs
+                msg
